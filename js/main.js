05a4701652cc90bbaba5d48284b291f60964180a
KevoCodo <kevthedev00@gmail.com>
2020-02-15 13:34:03 -0500

    Adding more images and links

--- js/main.js
+++ js/main.js
@@ -65,3 +65,20 @@ AOS.init({
 	duration: 1000,
 	once: true
 });
+
+
+/*============Scroll to Top Function*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
